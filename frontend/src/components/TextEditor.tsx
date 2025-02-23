165bfca3a87e8c4ec68cfbf84e9df9eccb275d32
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-02-23 08:03:05 +0530

    Adding journal

--- frontend/src/components/TextEditor.tsx
+++ frontend/src/components/TextEditor.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import "../journal.css";
+import ReactMarkdown from 'react-markdown'
 
 const TextEditor: React.FC = () => {
-  return <div>TextEditor Page</div>;
+  return (<div>
+      <div id="parentContainer">
+        <div id="displayDiv">
+
+        </div>
+
+        <div id="textDiv">
+          <div id="optonsDiv">
+            <ul id="optionsList">
+              <a className="fileButton" id="save">SAVE</a>
+              <a className="fileButton" id="file">OPEN</a>
+              <a className="fileButton" id="preview">PREVIEW</a>
+              <a className="fileButton" id="clear">CLEAR</a>
+            </ul>
+          </div>
+          <div id="typingDiv">
+            <textarea id="typingSpace" placeholder="Go crazy here . . ."></textarea>
+          </div>
+
+          <div id="previewDiv">
+            <ReactMarkdown></ReactMarkdown>
+          </div>
+
+        </div>
+
+      </div>
+
+  </div>
+  );
 };
 
 export default TextEditor;
